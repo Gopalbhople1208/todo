e72bb86deb8300cf1fdcc7123353a201a656e790
Gopalbhople <gbhople63@gmail.com>
2026-03-12 23:15:40 +0530

    the input data is show in console add in both login and signup page

--- fronted/src/components/login.jsx
+++ fronted/src/components/login.jsx
@@ -1,22 +1,29 @@
 import {Link} from 'react-router-dom';
 import '../style/AddTask.css';
+import { useState } from "react";
 
 function Login(){
+
+    const [userData,setTaskData] = useState();
 
     return(
         <div className="container">
             <h1>User Login</h1>
             <label>User Email</label>
            
-            <input type="text" placeholder="Enter Email"></input>
+            <input onChange={(event)=>setTaskData({...userData,email:event.target.value})}  type="text" placeholder="Enter Email"></input>
             <br></br>
             <label>User Password</label>
-           
-            <input type="text" placeholder="Enter Password"></input>
+        
+            <input
+             onChange={(event)=>setTaskData({...userData,password:event.target.value})}
+            type="text" placeholder="Enter Password"></input>
             <br></br>
-            <button type="submit">Login</button>
+            <button 
+             onClick={()=>console.log(userData)}
+            type="submit">Login</button>
            
-           <Link to="/signup" >Sign Up</Link>
+           <Link className="link" to="/signup" >Sign Up</Link>
         </div>
     )
 
--- fronted/src/components/signup.jsx
+++ fronted/src/components/signup.jsx
@@ -1,23 +1,32 @@
 import {Link} from 'react-router-dom';
 import '../style/AddTask.css';
-
+import {useState} from 'react';
 function Signup(){
     
+        const [userData,setTaskData] = useState();
     return(
         <div className="container">
             <h1>Sign Up</h1>
             <label>User Name</label>
-            <input type="text" placeholder="Enter User Name"></input>
+            <input 
+            onChange={(event)=>setTaskData({...userData,name:event.target.value})} 
+            type="text" placeholder="Enter User Name"></input>
             <br></br>
             <label>Email</label>
-            <input type="text" placeholder="Enter Email "></input>
+            <input
+             onChange={(event)=>setTaskData({...userData,email:event.target.value})}
+            type="text" placeholder="Enter Email "></input>
             <br></br>
             <label>Password</label>
-            <input type="text" placeholder="Enter Password"></input>
+            <input 
+             onChange={(event)=>setTaskData({...userData,password:event.target.value})}
+            type="text" placeholder="Enter Password"></input>
             <br></br>
-            <button type="submit">Sign Up</button>
+            <button 
+            onClick={()=>console.log(userData)}
+           type="submit">Sign Up</button>
             <br></br>
-            <Link to="/login">Login</Link>
+            <Link  className="link" to="/login">Login</Link>
         </div>
     )
 }
