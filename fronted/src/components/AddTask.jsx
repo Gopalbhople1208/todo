cc1d1a5e3375118e155f6f376087defef1adbd17
Gopalbhople <gbhople63@gmail.com>
2026-03-12 14:16:58 +0530

    this code is add the useState ,setTaskData ,taskData and fetch to get backend link and the data of fronted is converinto API from by useing json.strigify

--- fronted/src/components/AddTask.jsx
+++ fronted/src/components/AddTask.jsx
@@ -1,32 +1,61 @@
+import { useState } from "react";
 import "../style/AddTask.css";
 
 function AddTask() {
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: ""
+  });
+
+  const handAddTask = async () => {
+    console.log(taskData);
+
+    let result = await fetch("http://localhost:3232/add-task", {
+      method: "POST",
+      body: JSON.stringify(taskData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    result = await result.json();
+
+    if (result) {
+      console.log("Add New Task");
+    }
+  };
+
   return (
     <div className="container">
       <h1>Add New Task</h1>
-      <form action="/" method="post">
-        <label htmlFor="title">Title</label>
-        <input 
-          type="text" 
-          placeholder="Enter The Title" 
-          name="title"
-          id="title"
-        />
-        <br />
 
-        <label htmlFor="description">Description</label>
-        <textarea 
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        placeholder="Enter The Title"
+        name="title"
+        id="title"
+        onChange={(event) =>
+          setTaskData({ ...taskData, title: event.target.value })
+        }
+      />
+      <br />
+
+      <label htmlFor="description">Description</label>
+      <textarea
         rows={4}
-          name="description" 
-          placeholder="Enter The Description"
-          id="description"
-        ></textarea>
-        <br />
+        name="description"
+        placeholder="Enter The Description"
+        id="description"
+        onChange={(event) =>
+          setTaskData({ ...taskData, description: event.target.value })
+        }
+      ></textarea>
+      <br />
 
-        <button className="submit">
-          Add Task
-        </button>
-      </form>
+      <button onClick={handAddTask} className="submit">
+        Add Task
+      </button>
     </div>
   );
 }
