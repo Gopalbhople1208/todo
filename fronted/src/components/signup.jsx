8ca51fcea42d131a0c51b70e321696104d76ad47
Gopalbhople <gbhople63@gmail.com>
2026-03-13 15:48:46 +0530

    add signup data to mongodb and add the token in cookies

--- fronted/src/components/signup.jsx
+++ fronted/src/components/signup.jsx
@@ -1,9 +1,37 @@
-import {Link} from 'react-router-dom';
+import {Link,useNavigate} from 'react-router-dom';
 import '../style/AddTask.css';
 import {useState} from 'react';
 function Signup(){
     
-        const [userData,setTaskData] = useState();
+        const [userData,setTaskData] = useState({ });
+
+        const navigate = useNavigate();
+
+const handSignUp = async () => {
+
+  console.log("user data sending.....",userData);
+
+  let response = await fetch('http://localhost:3232/signup',{
+      method:"POST",
+      headers:{
+          'Content-Type':'application/json'
+      },
+      body: JSON.stringify(userData)
+  });
+
+  let result = await response.json();
+
+  if(response.status === 201 && result.token){
+      document.cookie = "token=" + result.token + "; path=/";
+      console.log("response send", result);
+
+      alert("Signup successful");
+      navigate("/login");
+  }else{
+      alert("Signup failed");
+  }
+}
+
     return(
         <div className="container">
             <h1>Sign Up</h1>
@@ -23,7 +51,7 @@ function Signup(){
             type="text" placeholder="Enter Password"></input>
             <br></br>
             <button 
-            onClick={()=>console.log(userData)}
+            onClick={handSignUp}
            className="link" type="submit">Sign Up</button>
             <br></br>
             <Link to="/login">Login</Link>
